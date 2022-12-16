0aa4ce2790a2ed39e86ec36527346ef7ed230287
GH-Pedro <pmap@ua.pt>
2022-12-16 18:11:26 +0000

    Alteração na página incial

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ $("document").ready(function () {
   const carousel = new bootstrap.Carousel("#myCarousel", {
     interval: 10000,
   });
+$('#myCarousel').on('slide.bs.carousel', function(event) {
+  if (event.relatedTarget.id === 'Slide2' || event.relatedTarget.id === 'Slide3') {  // Dependendo do slide a navbar pode precisar de ocupacidade
+    $('#neubar').addClass('bg-opacity-75');
+    $('#neubar').removeClass('bg-opacity-10');
+  }
+
+  else{
+    $('#neubar').addClass('bg-opacity-10');
+    $('#neubar').removeClass('bg-opacity-75');
+  }
+  });
 });
+
